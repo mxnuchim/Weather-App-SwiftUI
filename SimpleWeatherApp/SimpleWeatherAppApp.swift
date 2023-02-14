b00af2f14c2c1f7aa8974c6d27fa41f17fe6006f
mxnuchim <manuchimoliver779@gmail.com>
2023-02-15 00:26:57 +0100

    Initial Commit

---
+++ SimpleWeatherApp/SimpleWeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleWeatherAppApp.swift
+//  SimpleWeatherApp
+//
+//  Created by Manuchim Oliver on 15/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct SimpleWeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
